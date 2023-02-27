bc9fa2e23dde2edccb3a37f644cbe6f89ae7d66f
Fatema7843 <fatematuz784@gmail.com>
2023-02-27 14:13:32 +0600

    2c

---
+++ 2nd lab/simple message.asm
@@ -0,0 +1,19 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+MSG DB 0AH,0DH,'Fatema Tuj Zuhura is a good girl$'
+
+.CODE
+
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    LEA DX,MSG
+    MOV AH,9
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
